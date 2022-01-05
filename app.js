519c638d987da687b2c8bf5ae734dae1939a0a8b
Anuj Singh <singhanuj620@gmail.com>
2022-01-06 00:21:28 +0530

    added basic middleware and swagger docs configs

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require('express')
 require('dotenv').config();
 
 const app = express();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
+// For Swagger Documentation
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+// Regular Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookies and File middleware
+app.use(cookieParser());
+app.use(fileUpload());
+
+// Morgan Middleware
+app.use(morgan("tiny"));
+
+// import all routes
+const home = require('./routes/home')
+
+// router middleware
+app.use("/api/v1", home)
 
 module.exports = app;
