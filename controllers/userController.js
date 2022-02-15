1b400df6be6aaebbf0b14407e25c1459b08b6f29
Anuj Singh <singhanuj620@gmail.com>
2022-02-15 17:15:13 +0530

    added logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,3 +56,13 @@ exports.login = BigPromise(
         cookieToken(user, res)
     }
 )
+
+exports.logout = BigPromise(
+    async (req, res, next) => {
+        res.cookie('token', null, { expires: new Date(Date.now()), httpOnly: true })
+        res.status(200).json({
+            success: true,
+            message: 'Logged out successfully'
+        })
+    }
+)
